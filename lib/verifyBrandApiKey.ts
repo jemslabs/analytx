cea702eb4027393b486358e48816545407af47c8
Krish Soni <isonikrish@gmail.com>
2025-12-22 00:18:46 +0530

    fix: added some logs to fix bug

--- lib/verifyBrandApiKey.ts
+++ lib/verifyBrandApiKey.ts
@@ -4,14 +4,14 @@ import { hashApiKey } from "./tools";
 export async function verifyBrandApiKey(req: Request) {
   const auth = req.headers.get("authorization");
   const apiKey = auth?.replace("Bearer ", "");
-
+  console.log(apiKey)
   if (!apiKey) return null;
 
   const hashed = hashApiKey(apiKey);
-
+console.log(hashed)
   const brand = await prisma.brandProfile.findFirst({
     where: { apiKey: hashed },
   });
-
+console.log(brand)
   return brand;
 }
